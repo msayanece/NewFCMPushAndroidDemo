2abab51d244bc3782c5d23708d7d8a890135e3e1
msayanece <msayanece@gmail.com>
2021-02-24 17:22:30 +0530

    Initial commit

---
+++ app/src/main/java/com/sayan/fcmpushdemo/MyFirebaseMessagingService.java
@@ -0,0 +1,54 @@
+package com.sayan.fcmpushdemo;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "MyFirebaseMessagingServ";
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        Log.d(TAG, "onNewToken: " + s);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+//                scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+//                handleNow();
+            }
+
+        }else{
+            String title = remoteMessage.getNotification().getTitle();
+            String body = remoteMessage.getNotification().getBody();
+
+            Log.d(TAG, "onMessageReceived: Notification : Title - " + title + " & body - " + body);
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+}
